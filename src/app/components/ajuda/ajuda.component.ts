e76c8b78e8f3c39eff4554cfc14711c574e6fea5
Paulo <prmanfredini@gmail.com>
2021-06-19 13:00:42 -0300

    pagina institutos e leis

--- src/app/components/ajuda/ajuda.component.ts
+++ src/app/components/ajuda/ajuda.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { LeisAPI } from './../../services/leis-api';
+import { Component, Input, OnInit } from '@angular/core';
+import { InstituicoesMulheresAPI } from 'src/app/services/instituicoes-mulheres-api';
 
 @Component({
   selector: 'app-ajuda',
@@ -6,6 +8,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./ajuda.component.css']
 })
 export class AjudaComponent implements OnInit {
+
+  @Input()  leis: LeisAPI | undefined
+  @Input()  institu: InstituicoesMulheresAPI | undefined
 
   constructor() { }
 
---
+++ src/app/components/leis/leis/leis.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-leis',
+  templateUrl: './leis.component.html',
+  styleUrls: ['./leis.component.css']
+})
+export class LeisComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
