68166d9a863d8988adc7bf8537a18b47605b347e
Paulo <prmanfredini@gmail.com>
2021-06-19 16:12:41 -0300

    form fixed

--- src/app/components/forms/forms.component.ts
+++ src/app/components/forms/forms.component.ts
@@ -9,6 +9,41 @@ export class FormsComponent implements OnInit {
 
   constructor() { }
 
+  nome : string = "";
+  contato : string = "";
+  texto : string = "";
+
+  setNome(evento: KeyboardEvent) {
+    this.nome = (<HTMLInputElement>evento.target).value;
+  }
+
+  getNome(){
+    return this.nome;
+  }
+
+  setTexto(evento: KeyboardEvent) {
+    this.texto = (<HTMLInputElement>evento.target).value;
+  }
+
+  getTexto() {
+    return this.texto;
+  }
+
+  setContato(evento: KeyboardEvent) {
+    this.contato = (<HTMLInputElement>evento.target).value;
+  }
+
+  getContato() {
+    return this.contato;
+  }
+
+  contatoNovo(evento1: MouseEvent) {
+
+    alert( "Mensagem enviada, {this.getNome()}, entraremos em contato. " );
+
+    }
+
+
   ngOnInit(): void {
   }
 
